8a9e0bd673ac6e4262293f7d574dad58ea1b0cde
lsh7d1 <lsh7d1@163.com>
2026-03-15 18:32:04 +0800

    sync

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -8,9 +8,7 @@
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -18,10 +16,22 @@ fn longest(x: &str, y: &str) -> &str {
     }
 }
 
+//fn longest(x: &String, y: &String) -> &String {
+//if x.len() > y.len() {
+//x
+//} else {
+//y
+//}
+//}
+
 fn main() {
     let string1 = String::from("abcd");
-    let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
+    let result = {
+        let string2 = "xyzxyz";
+        //longest(string1.as_str(), string2)
+        //let string2 = String::from("xyzxyz");
+        longest(string1.as_str(), string2)
+    };
     println!("The longest string is '{}'", result);
 }
